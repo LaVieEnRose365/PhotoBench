8c072454a0faeb245f692e2a5353097f098a8c55
Tianyi Xu <114067715+cloudofsorrow@users.noreply.github.com>
2026-05-14 14:19:37 +0800

    Add model paths configuration in config.py

---
+++ scripts/embedding/config.py
@@ -0,0 +1,14 @@
+model_paths = {
+    "ops_embedding": "PATH/TO/Ops-MM-embedding-v1-7B",
+    "vlm2vec": "PATH/TO/VLM2Vec_merged",
+    "rzenembed": "PATH/TO/RzenEmbed",
+    "QQMM-embed-v2": "PATH/TO/QQMM-embed-v2",
+    "B3_Qwen2_7B": "PATH/TO/B3_Qwen2_7B",
+    "Qwen2-VL-Embedding-8B": "PATH/TO/Qwen2-VL-Embedding-8B",
+    "Qwen2-VL-Embedding-2B": "PATH/TO/Qwen2-VL-Embedding-2B",
+    # "chinese-clip-vit-base-patch16": "PATH/TO/chinese-clip-vit-base-patch16",
+    # "chinese-clip-vit-large-patch14": "PATH/TO/chinese-clip-vit-large-patch14",
+    "clip-ViT-B-32-multilingual-v1": "PATH/TO/clip-ViT-B-32-multilingual-v1",
+    "siglip2-base-patch16-224": "PATH/TO/siglip2-base-patch16-224",
+    "siglip2-giant-opt-patch16-256": "PATH/TO/siglip2-giant-opt-patch16-256",
+}
